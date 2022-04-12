3122f06d516e2f6865a00905e5a092b345f34f84
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-04-12 18:04:27 -0500

    Add files via upload

---
+++ TuesdayApril12.js
@@ -0,0 +1,38 @@
+// Tuesday April 12
+// Video - 94 - 95 min 8:41
+// VIDEO 94 WAS NOTES  11 MIN LONG JUST SO YOU KNOW.
+console.log(me);
+// console.log(job);
+// console.log(year)
+
+
+var me = 'Jazmine';
+let job = 'unemployed';
+const year = 2010;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3)); 
+console.log(addArrow);
+console.log(addArrow(2, 3));
+
+function addDecl(a,b) {
+    return a + b;
+}
+
+const addExpr = function(a,b) {
+    return a + b;
+}
+
+var addArrow = (a,b) => a + b;
+
+// Example
+
+var numProducts = 10;
+
+
+function deleteShoppingCart() {
+  console.log('All products deleted!! :D')  
+}
+
+
